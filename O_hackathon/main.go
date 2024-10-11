ce364ca97b0c9655d40b892f42e09d90402b31d2
Daniel <kreiderdaniel10@gmail.com>
2024-10-11 17:44:55 +0300

    коммит версия 1

--- O_hackathon/main.go
+++ O_hackathon/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Нахождение n-го эллемента
 func repeatedNTimes(nums []int) int {
 	l := len(nums) / 2
 	mappy := make(map[int]int)
@@ -16,7 +17,22 @@ func repeatedNTimes(nums []int) int {
 	return 0
 }
 func main() {
-	a := []int{1, 2, 3, 3}
-	fmt.Println(repeatedNTimes(a))
+	fmt.Println("Введите количество элементов массива (должно быть кратно 2):")
+	var n int
+	fmt.Scan(&n)
+
+	// Проверка, что длина кратна 2
+	if n%2 != 0 {
+		fmt.Println("Ошибка: длина массива должна быть кратна 2.")
+		return
+	}
+
+	arr := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
+	}
+
+	fmt.Println(repeatedNTimes(arr))
 
 }
